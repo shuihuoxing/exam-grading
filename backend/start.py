77aac97f01adf7637daed01faac1e74d5f03470b
shuihuoxing <149672962+shuihuoxing@users.noreply.github.com>
2026-06-24 10:37:02 +0800

    Update start.py

--- backend/start.py
+++ backend/start.py
@@ -1,3 +1,6 @@
+import os
 import uvicorn
 
-uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 8000))
+print(f"Starting on port {port}")
+uvicorn.run("app.main:app", host="0.0.0.0", port=port)
